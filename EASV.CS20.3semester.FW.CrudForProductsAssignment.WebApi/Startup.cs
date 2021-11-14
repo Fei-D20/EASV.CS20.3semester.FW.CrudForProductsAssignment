412ed45538fbef25794ad0020d241f2e39d8148d
WojciechWirowski <71640545+WojciechWirowski@users.noreply.github.com>
2021-11-14 13:11:32 +0100

    Backend CRUD etc

---
+++ EASV.CS20.3semester.FW.CrudForProductsAssignment.WebApi/Startup.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EASV.CS20._3semester.FW.CrudForProductsAssignment.Core.IServices;
+using EASV.CS20._3semester.FW.CrudForProductsAssignment.Core.Models;
+using EASV.CS20._3semester.FW.CrudForProductsAssignment.Database;
+using EASV.CS20._3semester.FW.CrudForProductsAssignment.Domain.IRepositories;
+using EASV.CS20._3semester.FW.CrudForProductsAssignment.Domain.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace EASV.CS20._3semester.FW.CrudForProductsAssignment.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+            services.AddDbContext<ApplicationContext>(opt => opt.UseLoggerFactory(loggerFactory).UseSqlite("Data source = ProductProject.db"));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                        {Title = "EASV.CS20._3semester.FW.CrudForProductsAssignment.WebApi", Version = "v1"});
+            });
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("product-policy", builder =>
+                {
+                    builder
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "EASV.CS20._3semester.FW.CrudForProductsAssignment.WebApi v1"));
+            }
+            
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var ctx = scope.ServiceProvider.GetService<ApplicationContext>();
+                ctx.Database.EnsureDeleted();
+                ctx.Database.EnsureCreated();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            app.UseCors("product-policy");
+        }
+    }
+}
