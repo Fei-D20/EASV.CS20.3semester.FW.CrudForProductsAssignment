49166f06e2905885865f0c8d787fd4351137958d
Fei <evensnachi1@gmail.com>
2021-11-18 23:53:37 +0100

    front end

---
+++ FrontEnd/src/app/products/products-list/products-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-EASV-CS20-FW-Assignment-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
+})
+export class ProductsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
