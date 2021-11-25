48b45550a83fcde449954594c8e938a273baf9d3
Fei <evensnachi1@gmail.com>
2021-11-25 16:49:49 +0100

    add temp data

--- EASV.CS20.3semester.FW.CrudForProductsAssignment.DataBase/DbSeeder.cs
+++ EASV.CS20.3semester.FW.CrudForProductsAssignment.DataBase/DbSeeder.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using EASV.CS20._3semester.FW.CrudForProductsAssignment.Database.Entities;
 
 namespace EASV.CS20._3semester.FW.CrudForProductsAssignment.Database
@@ -25,6 +26,15 @@ namespace EASV.CS20._3semester.FW.CrudForProductsAssignment.Database
         public void SeedProduction()
         {
             _context.Database.EnsureCreated();
+            var count = _context.Products.Count();
+            if (count == 0)
+            {
+                _context.Products.Add(new ProductEntity() { Id = 1, Name = "Product001" });
+                _context.Products.Add(new ProductEntity() { Id = 2, Name = "Product002" });
+                _context.Products.Add(new ProductEntity() { Id = 3, Name = "Product003" });
+                _context.Products.Add(new ProductEntity() { Id = 4, Name = "Product004" });
+                _context.SaveChanges();
+            }
         }
     }
 }
